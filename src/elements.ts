e402a8ba35047511e5936af089927c3586521d62
Yaman KATBY <m.yaman.katby@gmail.com>
2019-11-06 18:37:28 +0300

    ✨ Add isElementsObject function to the elements class

--- src/elements.ts
+++ src/elements.ts
@@ -1,58 +1,61 @@
 import { insertAfter, insertBefore, pop, push, remove, replace, shift, unshift } from './functions';
+import { isElementsObject } from './isElementsObject';
 
 export class Elements<S = any> {
 	constructor(source?: S[]) {
 		if (source) this.source = source;
 	}
 
-	source: S[] = [];
+	public static isElementsObject = isElementsObject;
 
-	push = (...elements: S[]) => {
+	private source: S[] = [];
+
+	public push = (...elements: S[]) => {
 		this.source = push(this.source, ...elements);
 		return this;
 	};
-	unshift = (...elements: S[]) => {
+	public unshift = (...elements: S[]) => {
 		this.source = unshift(this.source, ...elements);
 		return this;
 	};
-	pop = (count = 1) => {
+	public pop = (count = 1) => {
 		this.source = pop(this.source, count);
 		return this;
 	};
-	shift = (count = 1) => {
+	public shift = (count = 1) => {
 		this.source = shift(this.source, count);
 		return this;
 	};
 
-	replace = (
+	public replace = (
 		predicate: number | ((element: S) => boolean),
 		callback: S | ((prevElement: S) => S),
 	) => {
 		this.source = replace(this.source, predicate, callback);
 		return this;
 	};
-	insertAfter = (
+	public insertAfter = (
 		predicate: number | ((element: S) => boolean),
 		...elements: S[]
 	) => {
 		this.source = insertAfter(this.source, predicate, ...elements);
 		return this;
 	};
-	insertBefore = (
+	public insertBefore = (
 		predicate: number | ((element: S) => boolean),
 		...elements: S[]
 	) => {
 		this.source = insertBefore(this.source, predicate, ...elements);
 		return this;
 	};
-	remove = (
+	public remove = (
 		predicate: number | number[] | ((element: S) => boolean),
 	) => {
 		this.source = remove(this.source, predicate);
 		return this;
 	};
 
-	toArray = () => this.source;
+	public toArray = () => this.source;
 }
 
 export const elements = <S = any>(source?: S[]) => new Elements(source);
