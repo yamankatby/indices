9ae5cb383e89fc577772f2b8fa3984bf00f7f8f1
Yaman KATBY <m.yaman.katby@gmail.com>
2019-11-08 12:05:41 +0300

    🏷 Add Typescript functions overload typings

--- src/functions.ts
+++ src/functions.ts
@@ -1,21 +1,37 @@
-export const push = <S = any>(source: S[], ...elements: S[]): S[] => {
+export function push<S = any>(source: S[], element: S): S[];
+export function push<S = any>(source: S[], ...elements: S[]): S[];
+export function push<S = any>(source: S[], ...elements: S[]): S[] {
 	return [...source, ...elements];
-};
-export const unshift = <S = any>(source: S[], ...elements: S[]): S[] => {
-	return [...elements, ...source];
-};
-export const pop = <S = any>(source: S[], count: number = 1): S[] => {
-	return source.slice(0, source.length - count);
-};
-export const shift = <S = any>(source: S[], count: number = 1): S[] => {
-	return source.slice(count);
-};
+}
 
-export const replace = <S = any>(
+export function unshift<S = any>(source: S[], element: S): S[];
+export function unshift<S = any>(source: S[], ...elements: S[]): S[];
+export function unshift<S = any>(source: S[], ...elements: S[]): S[] {
+	return [...elements, ...source];
+}
+
+export function pop<S = any>(source: S[]): S[];
+export function pop<S = any>(source: S[], count: number): S[];
+export function pop<S = any>(source: S[], count: number = 1): S[] {
+	return source.slice(0, source.length - count);
+}
+
+export function shift<S = any>(source: S[]): S[];
+export function shift<S = any>(source: S[], count: number): S[];
+export function shift<S = any>(source: S[], count: number = 1): S[] {
+	return source.slice(count);
+}
+
+export function replace<S = any>(source: S[], index: number, element: S): S[];
+export function replace<S = any>(source: S[], index: number, callbackfn: ((prevElement: S) => S)): S[];
+export function replace<S = any>(source: S[], predicate: ((element: S) => boolean), element: S): S[];
+export function replace<S = any>(source: S[], predicate: ((element: S) => boolean), callbackfn: ((prevElement: S) => S)): S[];
+export function replace<S = any>(source: S[], predicate: number | ((element: S) => boolean), callback: S | ((prevElement: S) => S)): S[];
+export function replace<S = any>(
 	source: S[],
 	predicate: number | ((element: S) => boolean),
 	callback: S | ((prevElement: S) => S),
-): S[] => {
+): S[] {
 	const index = typeof predicate === 'number' ? predicate : source.findIndex(predicate);
 	if (index === -1) {
 		return source;
@@ -23,33 +39,50 @@ export const replace = <S = any>(
 	// @ts-ignore
 	const element = typeof callback === 'function' ? callback(source[index]) : callback;
 	return [...source.slice(0, index), element, ...source.slice(index + 1)];
-};
-export const insertAfter = <S = any>(
+}
+
+export function insertAfter<S = any>(source: S[], index: number, element: S): S[];
+export function insertAfter<S = any>(source: S[], index: number, ...elements: S[]): S[];
+export function insertAfter<S = any>(source: S[], predicate: ((element: S) => boolean), element: S): S[];
+export function insertAfter<S = any>(source: S[], predicate: ((element: S) => boolean), ...elements: S[]): S[];
+export function insertAfter<S = any>(source: S[], predicate: number | ((element: S) => boolean), ...elements: S[]): S[];
+export function insertAfter<S = any>(
 	source: S[],
 	predicate: number | ((element: S) => boolean),
 	...elements: S[]
-): S[] => {
+): S[] {
 	const index = typeof predicate === 'number' ? predicate : source.findIndex(predicate);
 	if (index === -1) {
 		return source;
 	}
 	return [...source.slice(0, index + 1), ...elements, ...source.slice(index + 1)];
-};
-export const insertBefore = <S = any>(
+}
+
+export function insertBefore<S = any>(source: S[], index: number, element: S): S[];
+export function insertBefore<S = any>(source: S[], index: number, ...elements: S[]): S[];
+export function insertBefore<S = any>(source: S[], predicate: ((element: S) => boolean), element: S): S[];
+export function insertBefore<S = any>(source: S[], predicate: ((element: S) => boolean), ...elements: S[]): S[];
+export function insertBefore<S = any>(source: S[], predicate: number | ((element: S) => boolean), ...elements: S[]): S[];
+export function insertBefore<S = any>(
 	source: S[],
 	predicate: number | ((element: S) => boolean),
 	...elements: S[]
-): S[] => {
+): S[] {
 	const index = typeof predicate === 'number' ? predicate : source.findIndex(predicate);
 	if (index === -1) {
 		return source;
 	}
 	return [...source.slice(0, index), ...elements, ...source.slice(index)];
-};
-export const remove = <S = any>(
+}
+
+export function remove<S = any>(source: S[], index: number): S[];
+export function remove<S = any>(source: S[], indices: number[]): S[];
+export function remove<S = any>(source: S[], predicate: ((element: S) => boolean)): S[];
+export function remove<S = any>(source: S[], predicate: number | number[] | ((element: S) => boolean)): S[];
+export function remove<S = any>(
 	source: S[],
 	predicate: number | number[] | ((element: S) => boolean),
-): S[] => {
+): S[] {
 	return source.filter((element, index) =>
 		typeof predicate === 'number'
 			? index !== predicate
@@ -59,4 +92,4 @@ export const remove = <S = any>(
 				? !predicate.includes(index)
 				: true,
 	);
-};
+}
--- src/indices.ts
+++ src/indices.ts
@@ -9,52 +9,83 @@ export class Indices<S = any> {
 		if (source) this.source = source;
 	}
 
-	public push = (...elements: S[]) => {
+	public push(element: S): Indices<S>;
+	public push(...elements: S[]): Indices<S>;
+	public push(...elements: S[]) {
 		this.source = push(this.source, ...elements);
 		return this;
-	};
-	public unshift = (...elements: S[]) => {
+	}
+
+	public unshift(element: S): Indices<S>;
+	public unshift(...elements: S[]): Indices<S>;
+	public unshift(...elements: S[]) {
 		this.source = unshift(this.source, ...elements);
 		return this;
-	};
-	public pop = (count = 1) => {
+	}
+
+	public pop(): Indices<S>;
+	public pop(count: number): Indices<S>;
+	public pop(count = 1) {
 		this.source = pop(this.source, count);
 		return this;
-	};
-	public shift = (count = 1) => {
+	}
+
+	public shift(): Indices<S>;
+	public shift(count: number): Indices<S>;
+	public shift(count = 1) {
 		this.source = shift(this.source, count);
 		return this;
-	};
+	}
 
-	public replace = (
+	public replace(index: number, element: S): Indices<S>;
+	public replace(index: number, callbackfn: ((prevElement: S) => S)): Indices<S>;
+	public replace(predicate: ((element: S) => boolean), element: S): Indices<S>;
+	public replace(predicate: ((element: S) => boolean), callbackfn: ((prevElement: S) => S)): Indices<S>;
+	public replace(
 		predicate: number | ((element: S) => boolean),
 		callback: S | ((prevElement: S) => S),
-	) => {
+	) {
 		this.source = replace(this.source, predicate, callback);
 		return this;
-	};
-	public insertAfter = (
+	}
+
+	public insertAfter(index: number, element: S): Indices<S>;
+	public insertAfter(index: number, ...elements: S[]): Indices<S>;
+	public insertAfter(predicate: ((element: S) => boolean), element: S): Indices<S>;
+	public insertAfter(predicate: ((element: S) => boolean), ...elements: S[]): Indices<S>;
+	public insertAfter(
 		predicate: number | ((element: S) => boolean),
 		...elements: S[]
-	) => {
+	) {
 		this.source = insertAfter(this.source, predicate, ...elements);
 		return this;
-	};
-	public insertBefore = (
+	}
+
+	public insertBefore(index: number, element: S): Indices<S>;
+	public insertBefore(index: number, ...elements: S[]): Indices<S>;
+	public insertBefore(predicate: ((element: S) => boolean), element: S): Indices<S>;
+	public insertBefore(predicate: ((element: S) => boolean), ...elements: S[]): Indices<S>;
+	public insertBefore(
 		predicate: number | ((element: S) => boolean),
 		...elements: S[]
-	) => {
+	) {
 		this.source = insertBefore(this.source, predicate, ...elements);
 		return this;
-	};
-	public remove = (
+	}
+
+	public remove(index: number): Indices<S>;
+	public remove(indices: number[]): Indices<S>;
+	public remove(predicate: ((element: S) => boolean)): Indices<S>;
+	public remove(
 		predicate: number | number[] | ((element: S) => boolean),
-	) => {
+	) {
 		this.source = remove(this.source, predicate);
 		return this;
-	};
+	}
 
-	public toArray = () => this.source;
+	public toArray() {
+		return this.source;
+	}
 }
 
 export const indices = <S = any>(source?: S[]) => new Indices(source);
